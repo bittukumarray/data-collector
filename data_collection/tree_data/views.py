6e82f8835955a3e8715c38b4e24f5552570fbd2d
bittukumarray <raybittu242@gmail.com>
2020-12-19 14:49:10 +0530

    insert function completed

--- data_collection/tree_data/views.py
+++ data_collection/tree_data/views.py
@@ -20,12 +20,40 @@ class Device:
 		self.timeSpent=timeSpent 
 
 
-total_data={}
+total_data={"webReq":0, "timeSpent":0, "country":{}}
 
 
 class InsertData(APIView):
-    def post(self, req):
-    	dim=req.data["dim"]
-    	metrics=req.data["metrics"]
-    	return Response({"data":req.data})
+	def post(self, req):
+		dim=req.data["dim"]
+		metrics = req.data["metrics"]
+		dvc={}
+		cntry=""
+		for data in dim:
+			if data["key"]=="device":
+				dvc.update({"device":data["val"], "webReq":0, "timeSpent":0})
+			elif data["key"]=="country":
+				cntry=data["val"]
+		for data in metrics:
+			if data["key"]=="webreq":
+				dvc.update({"webReq":data["val"]})
+			elif data["key"]=="timespent":
+				dvc.update({"timeSpent":data["val"]})
+		
+		total_data["webReq"]+=dvc["webReq"]
+		total_data["timeSpent"]+=dvc["timeSpent"]
+		if cntry in total_data["country"].keys():
+			total_data["country"][cntry]["webReq"]+=dvc["webReq"]
+			total_data["country"][cntry]["timeSpent"]+=dvc["timeSpent"]
+			if dvc["device"] in total_data["country"][cntry]["device"]:
+				total_data["country"][cntry]["device"][dvc["device"]]["webReq"]+=dvc["webReq"]
+				total_data["country"][cntry]["device"][dvc["device"]]["timeSpent"]+=dvc["timeSpent"]
+			else:
+				total_data["country"][cntry]["device"].update({dvc["device"]:{"webReq":dvc["webReq"], "timeSpent":dvc["timeSpent"]}})
+
+		else:
+			total_data["country"].update({cntry:{"webReq":dvc["webReq"], "timeSpent":dvc["timeSpent"], "device":{dvc["device"]:{"webReq":dvc["webReq"], "timeSpent":dvc["timeSpent"]}}}})
+
+		print("yes")
+		return Response({"data":total_data})
         
